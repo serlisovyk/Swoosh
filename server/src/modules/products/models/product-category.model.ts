c4697db5b20ae4acac3a123b680ea46895398d6d
Serhii Lisovyk <serlesovik@gmail.com>
2026-03-14 17:21:10 +0200

    Rename products categories collection

--- server/src/modules/products/models/product-category.model.ts
+++ server/src/modules/products/models/product-category.model.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 
-@Schema({ timestamps: true })
+@Schema({ timestamps: true, collection: 'product_categories' })
 export class ProductCategory {
   _id!: string
 
